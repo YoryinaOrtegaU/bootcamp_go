93a0b427ceee8ec93f8862289f991fe7c1a4c0fe
YoryinaOrtegaU <yoryina.ortega@mercadolibre.com.co>
2023-03-29 17:21:45 -0500

    Actualizado hasta test

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	err "pratica/errorsp"
 	f "pratica/funciones"
 )
 
 func main() {
 
-	fmt.Println(f.CalcularImpuesto(51000))
+	fmt.Println(f.CalcularImpuesto(160000))
 
 	fmt.Println(f.CalcularPromedio(2, 3, 5, 5, 5, 4))
-	fmt.Println(f.CalcularSalario(15, "B"))
+
+	fmt.Println(f.CalcularSalario(50, "C"))
 
 	calculo := f.CalcularEstadisticas("promedio")
 	resultado := calculo(1, 2, 3, 4, 5)
@@ -20,4 +23,23 @@ func main() {
 	animal := f.Animal("gato")
 	fmt.Println(animal(5))
 
+	//errors
+
+	var salary int
+	salary = 120000
+	fmt.Println(err.CalcularImpuesto(salary))
+
+	e := CalcularImpuesto2(salary)
+	verificarError := errors.Is(e, error1)
+	fmt.Println(verificarError)
+
+}
+
+var error1 = errors.New("el salario es menor a 10.000")
+
+func CalcularImpuesto2(salary int) error {
+	if salary <= 10000 {
+		return error1
+	}
+	return nil
 }
